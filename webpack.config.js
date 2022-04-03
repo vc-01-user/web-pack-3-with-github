44bcde72cff5792e033a7e9ad61ac9fc6aace86f
Alex <alexander.prenzel@conventus.de>
2022-04-03 19:46:27 +0200

    inklude: css-loader & css-min-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -21,6 +22,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -33,6 +38,9 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     static: {
